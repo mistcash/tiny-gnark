fa7b82ece909c7545f928937ae5d932be850a6a6
Shramee Srivastav <shramee.srivastav@gmail.com>
2026-04-02 11:07:20 +0530

    feat: deferred poly ring checks

--- std/math/emulated/field.go
+++ std/math/emulated/field.go
@@ -94,9 +94,17 @@ func NewField[T FieldParams](native frontend.API) (*Field[T], error) {
 		api:              native,
 		log:              logger.Logger(),
 		constrainedLimbs: make(map[[16]byte]int),
-		checker:          rangecheck.New(native),
 		fParams:          newStaticFieldParams[T](native.Compiler().Field()),
+
+		deferredPolyChecker: &polyRingCheckManager[T]{},
 	}
+
+	// ring checks can add additional deferred checks, so run before other checks
+	native.Compiler().Defer(f.performDeferredRingChecks)
+
+	// rangecheck also adds deferred checks
+	f.checker = rangecheck.New(native)
+
 	if smallfields.IsSmallField(native.Compiler().Field()) {
 		compilelogger.LogOnce(native.Compiler(), zerolog.DebugLevel,
 			"emulated/isSmallField",
